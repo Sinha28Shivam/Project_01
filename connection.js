34efd82cb7c65403298603e1eb1494b09160e80e
sinha28shivam <sinha28.shivam@gmail.com>
2025-01-07 21:05:56 +0530

    Added mongodb databases

--- connection.js
+++ connection.js
@@ -2,12 +2,15 @@ const mongoose = require("mongoose");
 
 // Connect to MongoDB
 
-async function connectMOngoDb(url) {
-    return mongoose
-    .connect(url)
-    .then(() => console.log("Connected to MongoDB"))
-    .catch((err) => console.log("Error connecting to MongoDB", err));
-}
+async function connectMOngoDb() {
+    const url = "mongodb+srv://sinha28shivam:learningDatabaseDevShi@cluster0.i5bcz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+    try {
+      await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+      console.log("Connected to MongoDB Atlas");
+    } catch (err) {
+      console.error("Error connecting to MongoDB Atlas", err);
+    }
+  }
 
 module.exports = {
     connectMOngoDb
