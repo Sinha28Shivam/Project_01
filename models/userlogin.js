a3cd5bab52c1973ec204835cb0dedf44d2fa4196
sinha28shivam <sinha28.shivam@gmail.com>
2025-01-11 00:46:57 +0530

    Implemented encryption in password

--- models/userlogin.js
+++ models/userlogin.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const hashPassword = require("../middlewares/userlogin");
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -13,8 +14,18 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
+    },
+    role: {
+        type: String,
+        enum: ["admin", "user"],
+        default: "user"
     }
 }, { timestamps: true });
+
+// Hash password before saving
+userSchema.pre('save', hashPassword)
+
+
 
 const Userlogin = mongoose.model("Userlogin", userSchema);
 module.exports = Userlogin;
